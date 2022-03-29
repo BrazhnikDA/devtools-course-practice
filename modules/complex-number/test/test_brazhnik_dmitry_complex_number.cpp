76e236c26cc70c437f9aa20c549775db929a63c4
BrazhnikDA <M1ND228@yandex.ru>
2022-03-29 19:32:36 +0300

    Fix code style

--- modules/complex-number/test/test_brazhnik_dmitry_complex_number.cpp
+++ modules/complex-number/test/test_brazhnik_dmitry_complex_number.cpp
@@ -5,28 +5,28 @@
 #include "include/complex_number.h"
 
 TEST(BD_CN_TEST, TEST_COMPLEX_NUMBER_SUM_FIRST_AND_SECOND_TRUE) {
-	ComplexNumber complexNumber1(1, 2);
-	ComplexNumber complexNumber2(2, 1);
-	ComplexNumber complexNumberResult = complexNumber1 + complexNumber2;
+    ComplexNumber complexNumber1(1, 2);
+    ComplexNumber complexNumber2(2, 1);
+    ComplexNumber complexNumberResult = complexNumber1 + complexNumber2;
 
-	EXPECT_EQ(3, complexNumberResult.getRe());
-	EXPECT_EQ(3, complexNumberResult.getIm());
+    EXPECT_EQ(3, complexNumberResult.getRe());
+    EXPECT_EQ(3, complexNumberResult.getIm());
 }
 
 TEST(BD_CN_TEST, TEST_COMPLEX_NUMBER_MULTIPLY_FIRST_AND_SECOND_TRUE) {
-	ComplexNumber complexNumber1(5, 10);
-	ComplexNumber complexNumber2(3, 2);
-	ComplexNumber complexNumberResult = complexNumber1 * complexNumber2;
+    ComplexNumber complexNumber1(5, 10);
+    ComplexNumber complexNumber2(3, 2);
+    ComplexNumber complexNumberResult = complexNumber1 * complexNumber2;
 
-	EXPECT_EQ(-5, complexNumberResult.getRe());
-	EXPECT_EQ(40, complexNumberResult.getIm());
+    EXPECT_EQ(-5, complexNumberResult.getRe());
+    EXPECT_EQ(40, complexNumberResult.getIm());
 }
 
 TEST(BD_CN_TEST, TEST_COMPLEX_NUMBER_SUBTRACTION_FIRST_AND_SECOND_TRUE) {
-	ComplexNumber complexNumber1(5, 10);
-	ComplexNumber complexNumber2(3, 2);
-	ComplexNumber complexNumberResult = complexNumber1 - complexNumber2;
-	
-	EXPECT_EQ(2, complexNumberResult.getRe());
-	EXPECT_EQ(8, complexNumberResult.getIm());
+    ComplexNumber complexNumber1(5, 10);
+    ComplexNumber complexNumber2(3, 2);
+    ComplexNumber complexNumberResult = complexNumber1 - complexNumber2;
+
+    EXPECT_EQ(2, complexNumberResult.getRe());
+    EXPECT_EQ(8, complexNumberResult.getIm());
 }
