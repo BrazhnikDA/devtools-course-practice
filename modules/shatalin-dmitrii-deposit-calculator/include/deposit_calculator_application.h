8bde956e6b99c26bcaf604eba968487f61f579cf
BrazhnikDA <M1ND228@yandex.ru>
2022-05-13 21:11:37 +0300

    Fix

--- modules/shatalin-dmitrii-deposit-calculator/include/deposit_calculator_application.h
+++ modules/shatalin-dmitrii-deposit-calculator/include/deposit_calculator_application.h
@@ -1,20 +1,20 @@
 // Copyright 2022 Shatalin Dmitry
 
-#ifndef MODULES_SHATALIN_DMITRY_DEPOSIT_CALCULATOR_DEPOSIT_CALCULATOR_APPLICATION_H_
-#define MODULES_SHATALIN_DMITRY_DEPOSIT_CALCULATOR_DEPOSIT_CALCULATOR_APPLICATION_H_
+#ifndef MODULES_SHATALIN_DMITRII_DEPOSIT_CALCULATOR_INCLUDE_DEPOSIT_CALCULATOR_APPLICATION_H_
+#define MODULES_SHATALIN_DMITRII_DEPOSIT_CALCULATOR_INCLUDE_DEPOSIT_CALCULATOR_APPLICATION_H_
 
 #include <string>
 
 #include "include/deposit_calculator.h"
 
 class Application {
-private:
+private: 
     std::string getHelp(const std::string& appname);
     bool checkArgument(const std::string& str);
     int parseArgument(const char* arg);
 
-public:
+public: 
     std::string operator()(int argc, const char** argv);
 };
 
-#endif // MODULES_SHATALIN_DMITRY_DEPOSIT_CALCULATOR_DEPOSIT_CALCULATOR_APPLICATION_H_
+#endif  // MODULES_SHATALIN_DMITRII_DEPOSIT_CALCULATOR_INCLUDE_DEPOSIT_CALCULATOR_APPLICATION_H_
