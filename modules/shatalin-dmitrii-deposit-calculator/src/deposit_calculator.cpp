281658d517af0764fa7f4b8babc906de90e2ed79
BrazhnikDA <M1ND228@yandex.ru>
2022-06-10 18:40:04 +0300

    update

--- modules/shatalin-dmitrii-deposit-calculator/src/deposit_calculator.cpp
+++ modules/shatalin-dmitrii-deposit-calculator/src/deposit_calculator.cpp
@@ -7,6 +7,7 @@ double DepositCalculator::calculateDeposit(double amount, int years,
     if (amount < 0.0 || years < 0 || percent < 0.0) {
         throw std::invalid_argument("Enter the correct parameters!");
     }
+
     double result = amount;
     result += amount * percent / 100.0 * years;
     return result;
